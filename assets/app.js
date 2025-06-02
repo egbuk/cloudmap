90cc9d791f13b45273f944b048d82f33306ab72c
George <me@heymoon.cc>
2025-06-02 20:38:00 +0300

    Attribution control position fix

--- assets/app.js
+++ assets/app.js
@@ -6,19 +6,20 @@ const map = new maplibregl.Map({
     container: 'map',
     style: '/style',
     center: [0, 0],
-    zoom: 1
+    zoom: 1,
+    attributionControl: false
 });
-window.debugMap = map;
+map.addControl(new maplibregl.AttributionControl(), 'top-left');
 
 const rewind = document.getElementById('rewind');
 rewind.value = 0;
 const label = document.querySelector('#time label');
 label.innerText = new Date(rewind.dataset.time*1000)
-    .toLocaleTimeString().split(':').slice(0, 2).join(':');
+    .toTimeString().split(':').slice(0, 2).join(':');
 rewind.oninput = () => {
     const time = new Date(rewind.dataset.time * 1000 +
         rewind.value * 3600000);
-    label.innerText = time.toLocaleTimeString().split(':').slice(0, 2).join(':');
+    label.innerText = time.toTimeString().split(':').slice(0, 2).join(':');
     const clouds = map.getSource('clouds');
     clouds.setTiles(clouds.tiles.map((tile) => {
         const url = new URL(tile);
