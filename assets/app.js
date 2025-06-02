e9a89cee35edc669fd6a30b226c96ba918d92b3d
George <me@heymoon.cc>
2025-06-02 21:11:32 +0300

    Instant view update

--- assets/app.js
+++ assets/app.js
@@ -16,7 +16,7 @@ rewind.value = 0;
 const label = document.querySelector('#time label');
 label.innerText = new Date(rewind.dataset.time*1000)
     .toTimeString().split(':').slice(0, 2).join(':');
-rewind.oninput = () => {
+const oninput = () => {
     const time = new Date(rewind.dataset.time * 1000 +
         rewind.value * 3600000);
     label.innerText = time.toTimeString().split(':').slice(0, 2).join(':');
@@ -27,3 +27,10 @@ rewind.oninput = () => {
         return url.toString();
     }));
 };
+rewind.oninput = oninput;
+const nextHour = () => {
+    setTimeout(nextHour,3600000);
+    rewind.dataset.time = (parseInt(rewind.dataset.time)+3600).toString();
+    oninput();
+};
+setTimeout(nextHour,(rewind.dataset.time*1000+3600000)-new Date().getTime());
